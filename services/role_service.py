336324400ec637e56ccb986c478bb2a9b46b0ac0
StarStream <alexiswiesen@yahoo.com>
2026-02-26 12:32:47 +0100

    Create role_service.py

---
+++ services/role_service.py
@@ -0,0 +1,10 @@
+from utils.logger import logger
+
+
+class RoleService:
+    def __init__(self):
+        pass
+
+    # placeholder for future role logic
+    def debug(self):
+        logger.info("RoleService is initialized.")

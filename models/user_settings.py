4f207017786eedf9ed24191cb9cc80519c87671e
StarStream <alexiswiesen@yahoo.com>
2026-02-26 12:31:42 +0100

    Create user_settings.py

---
+++ models/user_settings.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, BigInteger, Boolean
+from .base import Base
+
+
+class UserSettings(Base):
+    __tablename__ = "user_settings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    guild_id = Column(BigInteger, index=True, nullable=False)
+    user_id = Column(BigInteger, index=True, nullable=False)
+
+    # placeholders for notification flags:
+    # notify_expedition = Column(Boolean, default=False)
+    # notify_stamina = Column(Boolean, default=False)
+    # notify_raid = Column(Boolean, default=False)

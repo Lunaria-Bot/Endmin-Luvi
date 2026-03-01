966a2b35daca1d756fe737c99f35b73532fb150c
StarStream <alexiswiesen@yahoo.com>
2026-03-01 15:55:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildIntegrations,
   ],
   makeCache: Options.cacheWithLimits({
     ...Options.DefaultMakeCacheSettings,

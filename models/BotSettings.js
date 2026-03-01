1bfb0363544feb84eb2663131aaaef112cab2f5f
StarStream <alexiswiesen@yahoo.com>
2026-03-01 16:25:49 +0100

    Update BotSettings.js

--- models/BotSettings.js
+++ models/BotSettings.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const botSettingsSchema = new mongoose.Schema({
   guildId: { type: String, required: true, unique: true },
 
-  t1RoleId: { type: String },
-  t2RoleId: { type: String },
-  t3RoleId: { type: String },
+  t1RoleId: { type: String, default: null },
+  t2RoleId: { type: String, default: null },
+  t3RoleId: { type: String, default: null },
 
-  raidResetPingChannelId: { type: String },
-  raidResetPingRoleId: { type: String },
-
-
+  raidResetPingChannelId: { type: String, default: null },
+  raidResetPingRoleId: { type: String, default: null }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('BotSettings', botSettingsSchema);

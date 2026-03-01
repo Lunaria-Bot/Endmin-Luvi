3d657e750cd4adfc388409c433227dc79a82f742
StarStream <alexiswiesen@yahoo.com>
2026-03-01 16:26:48 +0100

    Update Reminder.js

--- models/Reminder.js
+++ models/Reminder.js
@@ -6,13 +6,14 @@ const reminderSchema = new mongoose.Schema({
   cardId: { type: String },
   channelId: { type: String, required: true },
   remindAt: { type: Date, required: true, index: true },
-  type: { type: String, required: true, enum: ['expedition', 'stamina', 'raid', 'raid_spawn', 'card_drop'] },
-  reminderMessage: { type: String, required: true },
-
+  type: { 
+    type: String, 
+    required: true, 
+    enum: ['expedition', 'stamina', 'raid', 'raid_spawn', 'card_drop'] 
+  },
+  reminderMessage: { type: String, required: true }
+}, {
+  timestamps: true
 });
-
-
-// Unique index for stamina and raid reminders (userId + type)
-reminderSchema.index({ userId: 1, type: 1 }, { unique: true, partialFilterExpression: { type: { $in: ['stamina', 'raid', 'expedition', 'raid_spawn', 'card_drop'] } } });
 
 module.exports = mongoose.model('Reminder', reminderSchema);

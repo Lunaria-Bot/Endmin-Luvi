e47b447a63e2cb9b845ffeadd124ebdfd34ee14d
StarStream <alexiswiesen@yahoo.com>
2026-03-07 10:50:03 +0100

    Delete events/LilacBank.js

--- events/LilacBank.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const LilacBankSchema = new mongoose.Schema({
-  guildId: { type: String, required: true, unique: true },
-  cores: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('LilacBank', LilacBankSchema);
